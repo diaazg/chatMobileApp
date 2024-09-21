8d43e238c100b126d2d3a283d30e08470a6a41ce
diaazg <mohamedzellagui585@gmail.com>
2024-09-21 18:55:08 +0100

    use config.dart to store keys

--- lib/core/presentation/ui/screens/video_call_screen.dart
+++ lib/core/presentation/ui/screens/video_call_screen.dart
@@ -1,5 +1,5 @@
+import 'package:chat/config.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
 
 class VideoCallScreen extends StatelessWidget {
@@ -16,8 +16,8 @@ class VideoCallScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return ZegoUIKitPrebuiltCall(
         appSign:
-           dotenv.env['APP_SIGN']!,
-        appID: int.parse(dotenv.env['APP_ID']!),
+           Config.appSign,
+        appID: Config.appID,
         callID: roomID,
         userID: uid,
         userName: userName,
--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,19 @@ import 'package:chat/core/data/repo_imp/auth_repo_imp.dart';
 import 'package:chat/utils/classes/camera.dart';
 import 'package:chat/utils/classes/get_it.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
+void main() {
 
-void main()async {
+
+  // Setup dependencies
   setup();
+  
   final camera = getIt<Camera>();
   final authRepoImp = getIt<AuthRepoImp>();
   
-  WidgetsFlutterBinding.ensureInitialized();
+  // Get camera list
   camera.getCamerasList();
-  await dotenv.load(fileName: ".env");
-  
-  runApp( ChatApp(authRepoImp: authRepoImp));
-}
 
+  runApp(ChatApp(authRepoImp: authRepoImp));
+}
